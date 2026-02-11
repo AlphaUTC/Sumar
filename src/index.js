651e56d3ec4a1b5341c31302be358ed044fdc5f7
23JRD <alpha.utc.ia.2026.a@gmail.com>
2026-02-11 08:04:13 -0600

    mi primer commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Importación de librerias
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Configuración de puertos
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({
+        "Title": "Hola Mundo"
+    });
+});
+
+//comentario ejemplo 
+app.post('/sumar',(req,res) => {
+    const { num1,num2 } = req.body;
+
+
+
+//validar que se hayan enviado los dos numeros que no 
+if (!num1 || !num2) {
+    return res.status(400).send({error:'Faltan numeros para sumar'});
+
+}
+//sumar los numeros
+const resultado = num1 + num2;
+
+//enviar resultado
+res.send({resultado});
+});
+
+//Iniciando el servidor
+app.listen(app.get('port'), () => {
+    console.log("SERVIDOR EN EL PUERTO 3000");
+});
